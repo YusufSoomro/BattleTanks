8a227a3c48b270852fd1703fa01d76315731aa36
Yusuf Soomro <yusufblah@gmail.com>
2018-03-16 23:17:49 -0700

    Add PlayerController C++ class and logging for the actor which it's possessing

---
+++ BattleTanks/Source/BattleTanks/TankPlayerController.cpp
@@ -0,0 +1,22 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "BattleTanks.h"
+#include "TankPlayerController.h"
+
+void ATankPlayerController::BeginPlay() {
+	Super::BeginPlay();
+
+	ATank *ControlledTank;
+	ControlledTank = GetControlledTank();
+
+	if (!ControlledTank) {
+		UE_LOG(LogTemp, Warning, TEXT("Didn't get tank"));
+	}
+	else {
+		UE_LOG(LogTemp, Warning, TEXT("Got tank: %s"), *(ControlledTank->GetName()));
+	}
+}
+
+ATank* ATankPlayerController::GetControlledTank() const {
+	return Cast<ATank>(GetPawn());
+}
